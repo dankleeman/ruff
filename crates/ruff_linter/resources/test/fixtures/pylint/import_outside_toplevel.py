78f4147e7918a63c052a0a9463b685ae60ac7c36
Dan Kleeman <dan.kleeman.git@pm.me>
2023-10-28 01:11:07 -0500

    Updating from stash.

---
+++ crates/ruff_linter/resources/test/fixtures/pylint/import_outside_toplevel.py
@@ -0,0 +1,10 @@
+def print_python_version():
+    import sys
+
+    print(sys.version_info)
+
+import sys
+
+
+def print_python_version():
+    print(sys.version_info)
--- crates/ruff_linter/src/rules/pylint/rules/import_outside_toplevel.rs
+++ crates/ruff_linter/src/rules/pylint/rules/import_outside_toplevel.rs
@@ -1,5 +1,7 @@
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
+use ruff_python_ast::Stmt;
+use ruff_text_size::Ranged;
 
 use crate::checkers::ast::Checker;
 
